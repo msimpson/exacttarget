fbf6177b6dcc2c644b06536882dea8ee444df10a
Matthew Simpson <matt.simpson@alextom.com>
2011-05-08 08:55:49 -0400

    Remove nokogiri_to_hash (mistake).

--- lib/exacttarget.rb
+++ lib/exacttarget.rb
@@ -1,7 +1,6 @@
 # Nokogiri is required for XML parsing:
 begin
   require 'nokogiri'
-  require 'nokogiri_to_hash'
 rescue LoadError
   puts '[ExactTarget] Error: Nokogiri is missing, run "bundle install".'
 end
