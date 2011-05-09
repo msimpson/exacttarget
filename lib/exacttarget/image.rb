f57960c37a4d9eb996a42e61e347c2b40360cdaa
Matthew Simpson <matt.simpson@alextom.com>
2011-05-08 23:40:00 -0400

    Expand image_import function.
    TODO:
    FTP delete is not working (strange).
    Make image_import private and use helpers.

--- lib/exacttarget/image.rb
+++ lib/exacttarget/image.rb
@@ -3,9 +3,28 @@ module ExactTarget
   
     public
     
-    def image_import(files)
-      @files = (["#{files}"] if files.instance_of? String) || files
-      send(render(:image))
+    def image_import(file_path)
+      @name = File.basename(file_path)
+      
+      begin
+        put(file_path.to_s)
+        result = Nokogiri::XML(send(render(:image)))
+        delete(@name)
+      rescue Exception => msg
+        puts msg
+      end
+      
+      desc   = result.xpath('//filemanagement-description').text
+      total  = result.xpath('//filemanagement-totalmoved').text.to_i
+      name   = result.xpath('//filemanagement-info').text
+      error  = desc.include?('not exist') ? 'File not found.' : total < 1 ? 'Unsupported file type.' : nil
+      
+      {
+        :file_path => file_path,
+        :old_name  => @name,
+        :new_name  => name,
+        :error     => error
+      }
     end
     
   end
